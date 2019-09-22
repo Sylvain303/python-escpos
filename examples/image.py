15dae8c3197dda95219f8be3224e8dd167bb5475
Sylvain Viart <sylvain@opensource-expert.com>
2019-09-23 00:02:58 +0200

    revert byte for printing.

---
+++ examples/image.py
@@ -0,0 +1,22 @@
+import sys
+
+b = sys.path
+sys.path = [ '/home/pi/Printer/python-escpos/build/lib' ] + b
+
+from escpos.printer import Usb, File
+
+def usage():
+    print("usage: image.py <image_file>")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
+
+    content = sys.argv[1]
+
+    # Adapt to your needs
+    #p = Usb(0x0416, 0x5011, profile="POS-5890")
+    p = File("output")
+    p.image(content,  invert_byte=False)
---
+++ examples/myqrcode.py
@@ -0,0 +1,21 @@
+import sys
+
+b = sys.path
+sys.path = [ '/home/pi/Printer/python-escpos/build/lib' ] + b
+from escpos.printer import Usb, File
+
+
+def usage():
+    print("usage: myqrcode.py <content>")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit(1)
+
+    content = sys.argv[1]
+
+    # Adapt to your needs
+    p = File("output")
+    p.qr(content, size=10, center=True)
