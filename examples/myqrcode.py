6c57cd879f079b6c1055db8e89dbdd0aae7d6fdc
Sylvain Viart <sylvain@opensource-expert.com>
2019-09-25 18:25:59 +0200

    modified myqrcode add -r for chosing reverse bits

--- examples/myqrcode.py
+++ examples/myqrcode.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python3
 import sys
-
 import qrcode
 from escpos.printer import Usb, File
 from escpos.constants import ESC, GS, NUL, QR_ECLEVEL_L, QR_ECLEVEL_M, QR_ECLEVEL_H, QR_ECLEVEL_Q
 
-
 def usage():
-    print("usage: myqrcode.py <content> <dest_file>")
+    """
+    Usage: myqrcode.py [-r] <content> <dest_file>
+
+    Options:
+      -r    reverse bit order in bytes.
+
+    Arguments:
+      <dest_file>   a file name or - for stdout
+                    ex: /dev/usb/lp0
+    """
+    print(usage.__doc__.strip())
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         usage()
         sys.exit(1)
 
-    content = sys.argv[1]
-    dest    = sys.argv[2]
-
+    reverse = False
+    if sys.argv[1] == "-r":
+        reverse = True
+        content = sys.argv[2]
+        dest    = sys.argv[3]
+    else:
+        content = sys.argv[1]
+        dest    = sys.argv[2]
+        
     if dest == '-':
         dest = '/dev/stdout'
 
@@ -37,7 +51,7 @@ if __name__ == '__main__':
     print("qr_code_width: %d" % width_pixels, file=sys.stderr)
 
     p.text('\n')
-    p.image(im, center=True, invert_byte=True)
+    p.image(im, center=True, invert_byte=reverse)
     p.text('\n')
     p.text('\n')
     
